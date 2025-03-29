82295c0082cd4bc518bb8efc0931bf85031d7201
Wakamatu57 <raamentaishimasshu@gmail.com>
2025-03-29 16:39:34 +0900

    修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "タイトルがはいります",
-  description: "説明文がはいります",
+  title: 'タイトルがはいります',
+  description: '説明文がはいります',
 };
 
 export default function RootLayout({
